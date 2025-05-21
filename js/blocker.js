6f3df102f8a5b8e0345e72ce25cc7f3e6b636cba
zCarlinTheBesT <carlosgabriellimapc@gmail.com>
2025-05-21 04:18:52 -0300

    Add files via upload

---
+++ js/blocker.js
@@ -0,0 +1,1 @@
+function getKey(e){var n=e.keyCode;if(console.log(n),16!=n&&17!=n||(mode=2),1==mode){if(123==n)return!1}else{if(73==n||74==n||85==n)return!1;if(123==n)return!1}}mode=1,document.oncontextmenu=new Function("return false;"),window.onkeydown=getKey;
